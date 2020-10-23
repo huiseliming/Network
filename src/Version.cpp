8ba5ee67ca3154bcdc93f6547161d6c915a4a078
HuiSeLiMing <1055907692@qq.com>
2020-10-23 21:17:56 +0800

    3

---
+++ src/Version.cpp
@@ -0,0 +1,9 @@
+#include "Version.h"
+
+
+const std::string Version = GIT_VERSION;
+const std::string VersionMajor = GIT_VERSION_MAJOR;
+const std::string VersionMinor = GIT_VERSION_MINOR;
+const std::string VersionPatch = GIT_VERSION_PATCH;
+const std::string CommitDate = GIT_COMMIT_DATE;
+const std::string CommitSubject = GIT_COMMIT_SUBJECT;
